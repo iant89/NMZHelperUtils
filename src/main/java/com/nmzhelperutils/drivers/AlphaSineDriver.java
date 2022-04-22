13468fb68e1fc22b4dbdbdb555d94df23d5f1de3
iant89 <itjp89@gmail.com>
2022-04-22 17:23:34 -0400

    Fixed Formatting, Moved AlphaSineDriver Test to its own class.

--- src/main/java/com/nmzhelperutils/drivers/AlphaSineDriver.java
+++ src/main/java/com/nmzhelperutils/drivers/AlphaSineDriver.java
@@ -11,58 +11,6 @@ public class AlphaSineDriver extends ValueDriver<Float>{
 
 	private long nextStepTimer = -1;
 
-	public static void main(String[] args) {
-
-		// 30 = 1906
-		// 31 = 1968, 1953
-		AlphaSineDriver alphaDriver = new AlphaSineDriver(0.125f, 0.75f, 20);
-
-		System.out.println("Step = " + alphaDriver.getStep());
-
-		boolean minReached = false;
-		boolean maxReached = false;
-
-		long startTimer = System.currentTimeMillis();
-
-		StringBuilder sb = new StringBuilder();
-
-		float previousValue = Float.MAX_VALUE;
-		while(true) {
-			float value = alphaDriver.getValue();
-
-			if(value == alphaDriver.getMaximum()) {
-				maxReached = true;
-			} else if(value == alphaDriver.getMinimum()) {
-				minReached = true;
-			}
-
-			if(minReached && maxReached) {
-				final long endTime = System.currentTimeMillis();
-				double seconds = ((endTime - startTimer) / 1000.0);
-				long milliseconds = endTime - startTimer;
-				if(seconds < 1) {
-					sb.append("Done, Took " + seconds + " Seconds\n\n");
-				} else {
-					sb.append("Done, Took " + milliseconds + " Milliseconds.");
-				}
-
-				break;
-			}
-
-			if(previousValue != Float.MAX_VALUE) {
-				if(previousValue != value) {
-					sb.append("" + value + "\n");
-					previousValue = value;
-				}
-			} else {
-				previousValue = value;
-			}
-		}
-
-		System.out.println("" + sb.toString());
-
-	}
-
 	public AlphaSineDriver(float min, float max, long duration) {
 		this.minimum = min;
 		this.maximum = max;
--- src/test/java/com/nmzhelperutils/UltimateNMZPluginTest.java
+++ src/test/java/com/nmzhelperutils/UltimateNMZPluginTest.java
@@ -3,10 +3,9 @@ package com.nmzhelperutils;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class UltimateNMZPluginTest
-{
-	public static void main(String[] args) throws Exception
-	{
+public class UltimateNMZPluginTest {
+
+	public static void main(String[] args) throws Exception {
 		ExternalPluginManager.loadBuiltin(UltimateNMZPlugin.class);
 		RuneLite.main(args);
 	}
